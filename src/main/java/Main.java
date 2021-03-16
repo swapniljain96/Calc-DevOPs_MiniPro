0909355f9364014336a5afcc9de33a428f94ae9b
swapniljain96 <swapnil.jain@iiitb.org>
2021-03-16 15:23:41 +0530

    Final Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,26 +10,30 @@ public class Main {
     private static final Logger Logger = LogManager.getLogger(Main.class);
 
     public static double squareroot(double number1) {
-        Logger.info("SQUARE_ROOT - Input:" + number1 + " - Output:" + Math.sqrt(number1));
+        Logger.info("Calculating square root  of given number:" + number1);
+        Logger.info("Resultant answer of power operations is : " + Math.sqrt(number1));
         return Math.sqrt(number1);
     }
 
     public static double factorials(double number1) {
+        Logger.info("Calculating factorial  of given number:" + number1);
         int i, fact = 1;
         for (i = 1; i <= number1; i++) {
             fact = fact * i;
         }
-        Logger.info("FACTORIAL - Input:" + number1 + " - Output:" + fact);
+        Logger.info("Resultant answer of power operations is : " + fact);
         return fact;
     }
 
     public static double naturallog(double number1) {
-        Logger.info("LOGRATHMIC  - Input:" + number1 + " - Output:" + Math.log(number1));
+        Logger.info("Calculating natural log  of given number:" + number1);
+        Logger.info("Resultant answer of natural log operation is : " + Math.log(number1));
         return Math.log(number1);
     }
 
     public static double powerfun(double number1, double number2) {
-        Logger.info("POWER_FUNTION  - Input:" + number1 + "^" + number2 + " - Output:" + Math.pow(number1, number2));
+        Logger.info("Calculating power function of given numbers:" + number1+","+number2);
+        Logger.info("Resultant answer of power operation is : " + Math.pow(number1, number2));
         return Math.pow(number1, number2);
     }
 
@@ -48,24 +52,31 @@ public class Main {
             ch = reader.nextInt();
             switch (ch) {
                 case 1:
+                    System.out.println("---------------------------------------------------");
                     System.out.println("Square root");
-                    System.out.println("Enter number");
+                    System.out.print("Enter number: ");
                     var1 = reader.nextDouble();
                     System.out.println("Squre root of " + var1 + " is : " + squareroot(var1));
+                    System.out.println("---------------------------------------------------");
                     break;
                 case 2:
+                    System.out.println("---------------------------------------------------");
                     System.out.println("Factorial");
-                    System.out.println("Enter number");
+                    System.out.print("Enter number: ");
                     var1 = reader.nextDouble();
                     System.out.println("Factorial of " + var1 + " is: " + factorials(var1));
+                    System.out.println("---------------------------------------------------");
                     break;
                 case 3:
+                    System.out.println("---------------------------------------------------");
                     System.out.println("Natural Logarithim");
-                    System.out.println("Enter number");
+                    System.out.print("Enter number: ");
                     var1 = reader.nextDouble();
                     System.out.println("Natural Logarithm of " + var1 + " is: " + naturallog(var1));
+                    System.out.println("---------------------------------------------------");
                     break;
                 case 4:
+                    System.out.println("---------------------------------------------------");
                     System.out.println("Power function");
                     System.out.println("Enter two numbers");
                     System.out.print("Enter number 1: ");
@@ -73,6 +84,7 @@ public class Main {
                     System.out.print("Enter number 2: ");
                     var2 = reader.nextDouble();
                     System.out.println(var1 + "^" + var2 + " is : " + powerfun(var1, var2));
+                    System.out.println("---------------------------------------------------");
                     break;
                 case 5:
                     exit(0);
